fe3225a59f4c5fafd94b66f3cf7c03cdb4369840
Mauri <mau.lt7@hotmail.com>
2021-06-13 23:06:45 -0400

    inicio de TPfinal

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,39 @@
+class Main
+{
+    public getElementById(id:string):HTMLElement
+    {
+        return document.getElementById(id);
+    }
+    public getElementByIdvalue(id:string):any
+    {
+        return <any> document.getElementById(id).value;
+    }
+    public getElementByIdchecked(id:string):boolean
+    {
+        return <boolean> document.getElementById(id).checked;
+    }
 
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+
+
 }
+
+window.addEventListener("load",()=>
+{
+    let miObj:Main = new Main();
+    let button=miObj.getElementById("Add_dev");
+    
+    button.addEventListener("click",()=>
+    {
+        let input1=miObj.getElementByIdvalue("device");
+        let input2=miObj.getElementByIdvalue("description");
+        let checkbox1=miObj.getElementByIdchecked("check1");
+        let checkbox2=miObj.getElementByIdchecked("check2");
+        if ((input1!=0 && input2!=0 )&& (checkbox1!=false || checkbox2!=false)) {
+            alert("los inputs son: " + input1 + "  " + input2 + checkbox1 + checkbox2);
+        }
+        else 
+        {
+            alert ("llene los cuadros y seleccione los controladores para el dispositivo");
+        }
+    })
+})
